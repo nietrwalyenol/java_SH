8416b044c71b119fbf34f46df0d5a6c94e58ddab
nietrwalyenol <nietrwaly.enol@gmail.com>
2017-01-09 20:02:54 +0100

    Update BankManager.java

---
+++ src/main/java/com/example/shdemo/service/BankManager.java
@@ -0,0 +1,10 @@
+package com.example.shdemo.service;
+
+import java.util.List;
+
+import com.example.shdemo.domain.Fluid;
+import com.example.shdemo.domain.Donation;
+
+public interface BankManager {
+	
+}
