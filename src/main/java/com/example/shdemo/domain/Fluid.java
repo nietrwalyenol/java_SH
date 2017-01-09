5a53dc90b4691f3d9c0dee9b1e0ca7ee97a5dc4b
nietrwalyenol <nietrwaly.enol@gmail.com>
2017-01-09 20:00:25 +0100

    Update Fluid.java
    korekta błędu (kod w złym pliku)

--- src/main/java/com/example/shdemo/domain/Fluid.java
+++ src/main/java/com/example/shdemo/domain/Fluid.java
@@ -1,23 +1,14 @@
 package com.example.shdemo.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 @Entity
-@NamedQueries({ 
+@NamedQueries({
 
 })
 
