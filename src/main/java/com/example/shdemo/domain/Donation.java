a9aeda5e2fd34897201d95c4696ff5d7d097604e
nietrwalyenol <nietrwaly.enol@gmail.com>
2017-01-11 23:50:23 +0100

    Update Donation.java
    uzupełnienie tabel

--- src/main/java/com/example/shdemo/domain/Donation.java
+++ src/main/java/com/example/shdemo/domain/Donation.java
@@ -23,4 +23,29 @@ import javax.persistence.TemporalType;
 
 public class Donation {
 
+	private Long id;
+	private String place = "";
+	private Date donationDate = new Date();
+  private List<Fluid> fluids = new ArrayList<Fluid>();
+
+	public void setId(Long id) {
+		this.id = id;
+	}	
+  
+	public String getPlace() {
+		return place;
+	}
+  
+	public void setPlace(String place) {
+		this.place = place;
+	}
+  
+	public Date getDonationDate() {
+		return donationDate;
+	}
+  
+	public void setDonationDate(Date donationDate) {
+		this.donationDate = donationDate;
+	}
+
 }
