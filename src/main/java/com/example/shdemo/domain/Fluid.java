cf9f038901ce726b23ecbd426fba8b0ecd3c19c7
nietrwalyenol <nietrwaly.enol@gmail.com>
2017-01-11 23:41:59 +0100

    Update Fluid.java
    uzupełnienie tabel

--- src/main/java/com/example/shdemo/domain/Fluid.java
+++ src/main/java/com/example/shdemo/domain/Fluid.java
@@ -13,5 +13,47 @@ import javax.persistence.NamedQuery;
 })
 
 public class Fluid {
+  
+	private Long id;
+	private String type;
+	private int volume;
+  private int value;
+	private Boolean availability = false;
+  
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getVolume() {
+		return volume;
+	}
+
+	public void setVolume(String volume) {
+		this.volume = volume;
+	}
+  
+  public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Boolean getAvailability() {
+		return Availability;
+	}
+
+	public void setAvailability(Boolean availability) {
+		this.availability = availability;
+	}
 
 }
