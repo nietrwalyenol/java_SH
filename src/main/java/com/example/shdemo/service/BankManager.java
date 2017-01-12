1d0a11876586ba2a9947182f3f77f983f26c4f53
nietrwalyenol <nietrwaly.enol@gmail.com>
2017-01-12 17:09:48 +0100

    Update BankManager.java

--- src/main/java/com/example/shdemo/service/BankManager.java
+++ src/main/java/com/example/shdemo/service/BankManager.java
@@ -7,4 +7,21 @@ import com.example.shdemo.domain.Donation;
 
 public interface BankManager {
 	
+	void addDonation(Donation donation);
+	List<Donation> getAllDonations();
+	void deleteDonation(Donation donation);
+	Donation findDonationbyId(Long id);
+	public Donation findDonationbyPlace(String place);
+	public boolean editDonation(Donation donation);
+	
+	Long addNewFluid(Fluid fluid);
+	List<Fluid> getAllFluids();
+	void deleteFluid(Fluid fluid);
+	Fluid findFluidById(Long id);
+	public boolean editFluid(Fluid fluid);
+	
+	List<Fluid> getOwnedFluids(Donation donation);
+	List<Fluid> getFreeFluids();
+	void sellFluid(Long donationId, Long fluidId);
+	
 }
